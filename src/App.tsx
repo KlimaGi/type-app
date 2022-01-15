77db8b5f40638585824a58daa80cdc92be40f629
Giedre <giedre.klima@gmail.com>
2022-01-15 12:31:39 +0200

    some complex types

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
-  // let firstValue: string = 'Many;
-  // let firstValue: number = 34;
-  // let firstValue: boolean = fasle;
-  // let firstValue: number[] = [1, 36, 7];
-  let firstValue: Array<string> = ["1", "36", "7"];
+  // tuple
+  let aTuple: [string, number] = ["Liza", 34];
+  // enum
+  enum Codes {
+    first = 1,
+    second = 2,
+  }
+  // any
+  let firstName: any = 9;
+  // void
+  const warning = (): void => {
+    console.log("Warning");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The value {firstValue} is of {typeof firstValue} type!
+          The value {firstName} is of {typeof firstName} type!
         </p>
+        <Message />
       </header>
     </div>
   );
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <p>This is some message!!!</p>;
+};
+
+export default Message;
