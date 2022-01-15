28a747537640fcc3c1eb934185693a2c3e68db52
Giedre <giedre.klima@gmail.com>
2022-01-15 12:16:16 +0200

    init, and different types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // let firstValue: string = 'Many;
+  // let firstValue: number = 34;
+  // let firstValue: boolean = fasle;
+  // let firstValue: number[] = [1, 36, 7];
+  let firstValue: Array<string> = ["1", "36", "7"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The value {firstValue} is of {typeof firstValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
