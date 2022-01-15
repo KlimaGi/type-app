ebb989d117bd34d3eec0579abde58bf288f2d777
Giedre <giedre.klima@gmail.com>
2022-01-15 12:44:18 +0200

    functional component props type

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,12 @@ import "./App.css";
 import Message from "./Message";
 
 function App() {
-  // tuple
-  let aTuple: [string, number] = ["Liza", 34];
-  // enum
-  enum Codes {
-    first = 1,
-    second = 2,
-  }
-  // any
-  let firstName: any = 9;
-  // void
-  const warning = (): void => {
-    console.log("Warning");
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value {firstName} is of {typeof firstName} type!
-        </p>
-        <Message />
+
+        <Message message="This is a simple message" />
       </header>
     </div>
   );
